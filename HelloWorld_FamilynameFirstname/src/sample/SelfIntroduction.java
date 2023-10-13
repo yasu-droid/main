d0049b68f11657af5c3712ecb25df48f8e4e4ff6
yasu-droid <95845809+yasu-droid@users.noreply.github.com>
2023-10-13 23:29:18 +0900

    1-3 再提出

---
+++ HelloWorld_FamilynameFirstname/src/sample/SelfIntroduction.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String name = "安田　卓矢";
+		int age = 25;
+		String hobby = "テニス";
+		
+		/*
+		 * 自己紹介文を出力する処理
+		 */
+		
+		
+		
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}

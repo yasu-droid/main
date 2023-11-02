4d12b63de77df62879cef74381a3f83da2fb41f6
yasu-droid <95845809+yasu-droid@users.noreply.github.com>
2023-11-02 21:48:01 +0900

    課題1-22 新規提出

--- HelloWorld_FamilynameFirstname/src/exec/Exec.java
+++ HelloWorld_FamilynameFirstname/src/exec/Exec.java
@@ -5,8 +5,9 @@ public class Exec {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		//	メソッドの呼び出し
 		Proc Proc = new Proc();
-
+		//	countryに日本をいれて、出力するようにする
 		Proc.Proc1("");
 	}
 
--- HelloWorld_FamilynameFirstname/src/proc/Proc.java
+++ HelloWorld_FamilynameFirstname/src/proc/Proc.java
@@ -5,27 +5,28 @@ import java.time.format.DateTimeFormatter;
 
 public class Proc {
 	String country;
-	
+
 	String review = "この寿司はうまい";
 	String eat = "寿司は和食です";
-	
+
 	// 現在日時を取得
 	LocalDateTime date1 = LocalDateTime.now();
 
 	// 表示形式を指定
 	DateTimeFormatter dtformat = 
-		DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-	
-	String fdate1 = dtformat.format(date1); //表示形式+Stringに変換
-	
+			DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+	// 表示形式+Stringに変換
+	String fdate1 = dtformat.format(date1);
 	String now = "今の現在日時は" + fdate1 + "です";
-	
+	// あいさつ文を出力するメソッド
 	public void Proc1(String country) {
 		this.country = country;
-		
+		//	nullチェック、countryがnullもしくは空文字なら
 		if(country == null || country == ""){
+			//	警告文の出力
 			System.out.println("nullもしくは空の文字列です");
 		}else {
+			//	それ以外なら
 			String greet = "こんにちは!ここは" + country + "です！";
 			System.out.println(greet);
 			System.out.println(review);
@@ -33,8 +34,8 @@ public class Proc {
 			System.out.println(now);
 		}
 
-		
+
 	}
-	
+
 
 }
